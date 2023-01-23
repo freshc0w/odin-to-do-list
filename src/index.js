106ee1d81d247ecb896a7b7a52f29ed6fa2b6522
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-23 11:52:25 +0000

    Add sidebar functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+const barToggle = true;
+
+const sideBar = document.getElementById('sideBar');
+sideBar.addEventListener('mouseover', openSideBar);
+sideBar.addEventListener('mouseout', closeSideBar)
+
+function openSideBar() {
+    sideBar.style.width = '200px';
+}
+
+function closeSideBar() {
+    sideBar.style.width = '85px';
+}
+
+console.log('Hello World')
