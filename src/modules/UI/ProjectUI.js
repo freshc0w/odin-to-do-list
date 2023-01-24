f94d15382edf9873178c3272973cf2d43d45d8ce
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-24 13:05:45 +0000

    Style task containers with respective elem

---
+++ src/modules/UI/ProjectUI.js
@@ -0,0 +1,89 @@
+import Task from "../task";
+import Project from "../project";
+
+class TaskUI {
+
+    // Purpose of this class it to take in a task obj, and turn it into
+    // a div with all relevant information regarding the task.
+    // From left-to-right: [checkBox, name, details, priority, date, editIcon, binIcon]
+    constructor(taskObj) {
+        this.task = taskObj;
+    };
+    renderInfo(elem, addedClass, text='') {
+        const name = document.createElement(elem);
+        name.classList.add(addedClass);
+        name.textContent = text; // default empty str
+        
+        return name
+    };
+    createElements() {
+        const statusCheck = this.renderInfo('input', 'statusCheck');
+        statusCheck.setAttribute('type', 'checkbox');
+        const name = this.renderInfo('div', 'taskName', this.task.name);
+
+        // When clicked, pop up a form where the details of the task is displayed.
+        const detailsBtn = this.renderInfo('button', 'taskDetailsBtn', 'Details');
+
+        // Priority icon
+        const priority = this.renderInfo('div', 'statusPrio');
+        const priorityIcon = this.renderInfo('i', 'material-icons', 'warning');
+
+        // Change color based on priority of task -> ['high', 'medium', 'low']
+        priorityIcon.classList.add(this.task.priority)
+        priority.appendChild(priorityIcon);
+
+        // Just a mock date for now. Subject to change based on user input.
+        const timeElapsed = Date.now();
+        const today = new Date(timeElapsed);
+        const date = this.renderInfo('div', 'taskDate', `${today.toLocaleDateString()}`);
+
+        // icons
+        const edit = this.renderInfo('div', 'taskEdit');
+        const editIcon = this.renderInfo('i', 'material-icons', 'edit');
+        edit.appendChild(editIcon);
+
+        const taskDel = this.renderInfo('div', 'taskDel');
+        const delIcon = this.renderInfo('i', 'material-icons', 'delete');
+        taskDel.appendChild(delIcon);
+
+        return [statusCheck, name, detailsBtn, priority, date, edit, taskDel];
+    };
+    draw() {
+        const taskContainer = this.renderInfo('div', 'task-container');
+        const elems = this.createElements();
+        for (let elem of elems) {
+            taskContainer.appendChild(elem);
+        };
+        return taskContainer;
+    };
+};
+
+class ProjectUI {
+    constructor(projectObj) {
+        this.project = projectObj;
+    };
+    draw() {
+        const mainContent = document.querySelector('#main-content');
+        this.clear(mainContent);
+
+        // Loop through all the tasks and give it a TaskUI class.
+        // Use the draw method to get the div container and append it to mainContent.
+
+        this.drawTasks(mainContent, this.project.tasks);
+    }
+    drawTasks(container, tasks) {
+        for(let task of tasks) {
+            const taskDisplay = new TaskUI(task);
+            container.appendChild(taskDisplay.draw());
+        };
+    };
+
+    clear(content) {
+        content.innerHTML = '';
+    }
+};
+
+export {
+    TaskUI,
+    ProjectUI,
+}
--- src/modules/UI/ToDoUI.js
+++
@@ -1,2 +0,0 @@
-import Task from "../task";
-import Project from "../project";
