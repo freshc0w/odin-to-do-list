6a327b8bf9a60ba71876b39a2350c0a42b97ca69
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-31 09:15:26 +0000

    Revert optimisation for AllProject.js for dupl bug:

--- src/modules/AllProjects.js
+++ src/modules/AllProjects.js
@@ -1,58 +1,63 @@
+import Task from "./task";
 import Project from "./project";
 
 export default class AllProjects {
-	constructor() {
-		this.projects = [];
-		["Inbox", "Today", "This Week", "Important"].forEach((proj) => {
-			this.projects.push(new Project(proj));
-		});
-	}
+    constructor() {
+        this.projects = [];
+        for(let proj of ["Inbox", "Today", "This Week", "Important"]) {
+            this.projects.push(new Project(proj));
+        };
+    };
 
-	get projects() {
-		return this._projects;
-	}
-	set projects(value) {
-		this._projects = value;
-	}
+    get projects() {
+        return this._projects;
+    };
+    set projects(value) {
+        this._projects = value;
+    }
 
-	// Check if project name exists in list of projects.
-	contains(projectName) {
-		return this.projects.some((project) => project.name === projectName);
-	}
+    // Check if project name exists in list of projects.
+    contains(projectName) {
+        return this.projects.some(project => project.name === projectName);
+    };
 
-	getProject(projectName) {
-		return this.projects.find((project) => project.name === projectName);
-	}
+    getProject(projectName) {
+        return this.projects.find(project => project.name === projectName);
+    };
 
-	addProject(newProjectName) {
-		// Cannot have same project names.
-		if (this.contains(newProjectName)) return;
-		this._projects.push(new Project(newProjectName));
-	}
+    addProject(newProjectName) {
+        // Cannot have same project names.
+        if (this.contains(newProjectName)) return;
+        this._projects.push(new Project(newProjectName));
+    };
 
-	deleteProject(projectName) {
-		const projectToDelete = this.projects.find(
-			(project) => project.name === projectName
-		);
-		this.projects.splice(this.projects.indexOf(projectToDelete), 1);
-	}
-	getAllTasksToday() {
-		// Iterate thru each project and use getTasksToday() on each.
-		return this.projects.reduce(
-			(acc, project) => [...acc, ...project.getTasksToday()],
-			[]
-		);
-	}
-	getAllTasksThisWeek() {
-		return this.projects.reduce(
-			(acc, project) => [...acc, ...project.getTasksThisWeek()],
-			[]
-		);
-	}
-	getAllImportantTasks() {
-		return this.projects.reduce(
-			(acc, project) => [...acc, ...project.getImportantTasks()],
-			[]
-		);
-	}
-}
+    deleteProject(projectName) {
+        const projectToDelete = this.projects.find(project => 
+                                project.name === projectName);
+        this.projects.splice(this.projects.indexOf(projectToDelete),
+                                1)
+    };
+    getAllTasksToday() {
+        // Iterate thru each project and use getTasksToday() on each.
+        const tasksToday = [];
+        for(let tasks of this.getProject("Inbox").getTasksToday()) {
+            tasksToday.push(tasks);
+        };
+        return tasksToday;
+    };
+    getAllTasksThisWeek() {
+        const tasksThisWeek = [];
+        for(let tasks of this.getProject("Inbox").getTasksThisWeek()) {
+            tasksThisWeek.push(tasks);
+        };
+        return tasksThisWeek;
+    };
+    getAllImportantTasks() {
+        const importantTasks = [];
+        for(let tasks of this.getProject("Inbox").getImportantTasks()) {
+            importantTasks.push(tasks);
+        };
+        return importantTasks
+    };
+    
+};
